20fed6e8ae8d935f6786c3cd6e9c7676f3452544
WoBen07 <benedict.wohlgemuth@uni-a.de>
2025-03-12 21:43:02 +0100

    Menu_Overlay

--- scripts/ui_overlay.gd
+++ scripts/ui_overlay.gd
@@ -6,6 +6,10 @@ extends Control
 @onready var heart_4: AnimatedSprite2D = $HeartContainer/Heart4
 @onready var coins_bank: Label = $coins_bank
 @onready var coins_add: Label = $coins_add
+@onready var menu: Button = $"../Menu"
+@onready var menu_2: Popup = $"../Menu2"
+@onready var close: Button = $"../Menu2/Panel/close"
+@onready var main_menu: Button = $"../Menu2/Panel/Main_menu"
 
 
 
@@ -15,6 +19,19 @@ func _ready() -> void:
 		Globals.player.connect("health_changed", _on_health_changed)
 	else:
 		print("Player not found")
+	menu.connect("pressed", _on_menu_pressed)
+	close.connect("pressed", _on_close_pressed)
+	main_menu.connect("pressed", _on_main_menu_pressed)
+	
+func _on_menu_pressed():
+	menu_2.visible = true
+	
+func _on_close_pressed():
+	menu_2.visible = false
+	
+func _on_main_menu_pressed():
+	LevelManager.load_main_menue()
+	
 func _on_health_changed(lives):
 	if Globals.player.max_lives == 4:
 		if lives == 3:
