93376a5443fcff87682e0e1a52deb5ac9724957a
WoBen07 <benedict.wohlgemuth@uni-a.de>
2025-03-15 21:34:36 +0100

    Bugfixes_Slime_grow_Defective

--- scripts/red_slime.gd
+++ scripts/red_slime.gd
@@ -14,6 +14,8 @@ func _ready() -> void:
 	original_scale = animated_sprite.scale
 
 func _process(delta: float) -> void:
+	if LevelManager.is_frozen:
+		speed = 10
 	# Move enemy properly
 	position.x += direction * speed * delta  
 
@@ -23,18 +25,18 @@ func _process(delta: float) -> void:
 
 	# Detect edges and turn around
 	if not down.is_colliding():
-		#flip_direction()
-		pass
+		flip_direction()
 
 func flip_direction():
+	# Move slightly away from the wall before flipping
+	move_local_x(direction * -5)  # Small push to avoid jitter
 	direction *= -1  # Reverse direction
 	animated_sprite.flip_h = not animated_sprite.flip_h  # Flip sprite
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
 		print("body entered")
 		animated_sprite.scale = original_scale * multiplier
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	if body.is_in_group("player"):
+		print("Body exited")
 		animated_sprite.scale = original_scale
